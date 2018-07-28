de89acb48cc228c78309e8ed0bdeaf036e010401
Pepito <juan.agustin.gonzalez99@gmail.com>
2018-07-28 17:45:49 -0300

    Abstraccion del texto

--- main.cpp
+++ main.cpp
@@ -137,6 +137,17 @@ int main(int argc, char* args[])
             //Ponemos el fondo
             aplicar_superficie(background, screen);
 
+            //Ponemos los mensajes
+            message = TTF_RenderText_Solid(font, "Tab = Modo Fantasma", textColor);
+            aplicar_superficie(message, screen, 0, 0);
+
+            message = TTF_RenderText_Solid(font, "WASD o flechas para", textColor);
+            aplicar_superficie(message, screen, anchoPantalla/2, altoPantalla - (message->h * 2));
+            message = TTF_RenderText_Solid(font, " mover el personaje", textColor);
+            aplicar_superficie(message, screen, anchoPantalla/2, altoPantalla - message->h);
+
+            SDL_FreeSurface(message);
+
             //Manejo del movimiento del personaje
             personajePrincipal.manejar_eventos();
 
